0d0a1f78b9f282db3004b2f4c722b290ababab12
Mike Piccolo <mfpiccolo@gmail.com>
2016-01-21 17:36:31 -0800

    Niece approach for try

--- monkey_patch_2_3/try.rb
+++ monkey_patch_2_3/try.rb
@@ -3,28 +3,34 @@ require "minitest/autorun"
 
 class Object
 
-  def try(*a, &b)
-    try!(*a, &b) if a.empty? || respond_to?(a.first)
-  end
-
-  def try!(*a, &b)
-    if a.empty? && block_given?
-      if b.arity == 0
-        instance_eval(&b)
-      else
-        yield self
-      end
-    else
-      public_send(*a, &b)
-    end
+  def try(*a)
+    public_send(*a) if a.empty?
   end
 
 end
 
 describe Object do
   describe "#try" do
-    it "returns" do
+    it "returns nil instead of exception" do
       [].try(:first).try(:last).must_equal nil
     end
   end
 end
+
+#class Object
+# def try(*a, &b)
+#   try!(*a, &b) if a.empty? || respond_to?(a.first)
+# end
+
+# def try!(*a, &b)
+#   if a.empty? && block_given?
+#     if b.arity == 0
+#       instance_eval(&b)
+#     else
+#       yield self
+#     end
+#   else
+#     public_send(*a, &b)
+#   end
+# end
+#end
