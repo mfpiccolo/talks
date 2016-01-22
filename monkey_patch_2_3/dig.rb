23c4aa91959d3118d1adc84b5aee33f4adfcec3c
Thomas Hopkins <thomas@fullstacklabs.co>
2016-01-21 17:58:50 -0800

    Add test for dig

--- monkey_patch_2_3/dig.rb
+++ monkey_patch_2_3/dig.rb
@@ -12,11 +12,16 @@ class Hash
 
 end
 
-describe Numeric do
+describe Hash do
   describe "#monkey_dig" do
     it "returns the nested value" do
       nested = {a: {b: {c: {d: 'f'}}}}
       nested.monkey_dig(:a, :b, :c, :d).must_equal 'f'
+    end
+
+    it "returns nil if any key in not present" do
+      nested = {a: {b: {e: {d: 'f'}}}}
+      nested.monkey_dig(:a, :b, :c, :d).must_equal nil
     end
   end
 end
