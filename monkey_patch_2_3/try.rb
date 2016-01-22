e0b7f9743129f29d29f99b82fa817bab65b1ef46
Mike Piccolo <mfpiccolo@gmail.com>
2016-01-21 17:46:09 -0800

    Another pass at naive try

--- monkey_patch_2_3/try.rb
+++ monkey_patch_2_3/try.rb
@@ -1,12 +1,18 @@
 require "minitest/autorun"
 # ruby -Ilib:test try.rb
 
+# class Object
+
+#   def try(a)
+#     public_send(a) if a.empty?
+#   end
+
+# end
+
 class Object
-
-  def try(a)
-    public_send(a) if a.empty?
+  def try(*a)
+    public_send(*a) if a.empty? || respond_to?(a.first)
   end
-
 end
 
 describe Object do
@@ -16,13 +22,13 @@ describe Object do
     end
   end
 
-  # describe "#try" do
-  #   it "returns nil instead of exception" do
-  #     {}.try(:[], :some_key).try(:last).must_equal nil
-  #   end
+  describe "#try" do
+    it "returns nil instead of exception" do
+      {}.try(:[], :some_key).try(:last).must_equal nil
+    end
 
-  #   it "returns the actual value for a passed in args" do
-  #     {some_key: "some_value"}.try(:[], :some_key).must_equal "some_value"
-  #   end
-  # end
+    it "returns the actual value for a passed in args" do
+      {some_key: "some_value"}.try(:[], :some_key).must_equal "some_value"
+    end
+  end
 end
