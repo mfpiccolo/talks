8dea8ffde8c160c7b3e41dba3282237bfa39a3ef
Thomas Hopkins <thomas@fullstacklabs.co>
2016-01-21 17:32:36 -0800

    Finish fetch_values

--- monkey_patch_2_3/fetch_values.rb
+++ monkey_patch_2_3/fetch_values.rb
@@ -5,7 +5,11 @@ class Hash
 
   def monkey_fetch_values(*keys)
     keys.map do |key|
-      self[key]
+      if self.has_key?(key)
+        self[key]
+      else
+        raise KeyError
+      end
     end
   end
 
@@ -13,9 +17,31 @@ end
 
 describe Numeric do
   describe "#monkey_fetch_values" do
+    before do
+      @hash = {a: 1, b: 2, c: 3, d: 4}
+    end
+
     it "returns an array of values for the given keys" do
-      hash = {a: 1, b: 2, c: 3, d: 4}
-      hash.monkey_fetch_values(:a, :b, :c, :d).must_equal [1,2,3,4]
+      @hash.monkey_fetch_values(:a, :b, :c, :d).must_equal [1,2,3,4]
+    end
+
+    it "returns an empty array if there are no arguments" do
+      @hash.monkey_fetch_values().must_equal []
+    end
+
+    it "raises error if a key is not present" do
+      proc { @hash.monkey_fetch_values(:d, :e) }.must_raise KeyError
+    end
+
+    it "doesn't raise an error if key is present with nil value" do
+      @hash[:e] = false
+      @hash[:f] = nil
+      @hash.monkey_fetch_values(:d, :e, :f).must_equal [4, false, nil]
+    end
+
+    it "doesn't mutate the hash" do
+      @hash.monkey_fetch_values(:a)
+      @hash.must_equal(a: 1, b: 2, c: 3, d: 4)
     end
   end
 end
